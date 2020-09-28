bb301a0f1061be1aba24619c01d553139f330377
Garrett Powell <garettbpowell@gmail.com>
2020-09-27 21:28:09 -0500

    things are looking up

--- app/src/main/java/com/fall2019/cs315/mediawebstuff/launch.java
+++ app/src/main/java/com/fall2019/cs315/mediawebstuff/launch.java
@@ -3,6 +3,8 @@ package com.fall2019.cs315.mediawebstuff;
 import android.app.Activity;
 import android.graphics.Movie;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -14,33 +16,42 @@ import android.widget.TextView;
 
 public class launch extends AppCompatActivity
 {
-
-    private MovieModel mItem;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
 
         Bundle extras;
         String newString = "";
-        if (savedInstanceState == null)
-        {
+        if (savedInstanceState == null) {
             extras = getIntent().getExtras();
-            if(extras == null)
-            {
+            if (extras == null) {
                 newString = null;
-            }
-            else
-            {
-                newString= extras.getString(MovieDetailFragment.ARG_ITEM_ID);
+            } else {
+                newString = extras.getString(MovieDetailFragment.ARG_ITEM_ID);
             }
         }
 
-            WebView thisWebView = findViewById(R.id.webView);
-            if(newString != null)
-                thisWebView.loadUrl(newString);
+        final WebView thisWebView = findViewById(R.id.webView);
+        thisWebView.getSettings().setJavaScriptEnabled(true);
+        if (newString != null) {
+            thisWebView.loadUrl(newString);
+            final TextView loading = findViewById(R.id.loading);
+            new Handler().postDelayed(new Runnable()
+            {
+                public void run ()
+                {
+                    showWebView(loading, thisWebView);
+                }
+            }, 2000);
         }
+    }
+    private void showWebView(TextView loading, WebView thisWebView)
+    {
+        loading.setVisibility(View.INVISIBLE);
+        thisWebView.setVisibility(View.VISIBLE);
+    }
 }
+
 
