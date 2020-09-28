170b25212cc69b74f430e6742a44c7ca904687f1
Zachary ElledgeMCGhee <zte18a@acu.edu>
2020-09-28 14:47:20 -0500

    Added 5 items to list. Movie, Game, Song, Youtuber, and app.

--- app/src/main/java/com/fall2019/cs315/mediawebstuff/DumbMovieContent.java
+++ app/src/main/java/com/fall2019/cs315/mediawebstuff/DumbMovieContent.java
@@ -22,41 +22,65 @@ public class DumbMovieContent {
      * Create all those movie Strings we will be needing for teh models
      */
 
-    // CS315: DO THIS
-    // TODO: Create five NEW movie objects here. Complete with images and URLs. DELETE the two existing movies, they are only here as an example
 
-    private static final String movie1Title = "Generic Rom Com:";
-    private static final String movie1Description = "Naturally, focuses on 2 people who really shouldn’t be together. It somehow becomes mutually advantageous for them to be together, or perhaps, they are interested in each other but their romance is based on false pretenses. Because the leads generally have no charisma, they each need a zany best friend who inserts dirty jokes wherever necessary. The two leads will fall in love in an extended music montage set to the music of Sixpence None the Richer. Once they find out about the false pretenses, they will take some time apart but realize how much they care for the other. Then comes “The Chase,” which can either be in an airport (not legal anymore), in traffic or somehow on boats. \n \nYou've seen this before, but not with these two actors!";
-    private static final String movie1Year = "2015";
-    private static final String movie1Image = "genericromcom";
-    private static final String movie1Weblink = "http://showtimeshowdown.com/5-cliche-formulas-of-movie-genres/";
+    private static final String fastTitle = "Fast and Furious Tokyo Drift";
+    private static final String fastDescription = "Fast cars, Guns, Explosions... What else do you need?";
+    private static final String fastYear = "2006";
+    private static final String fastImage = "fast";
+    private static final String fastWeblink = "https://www.imdb.com/title/tt0463985/";
 
-    private static final String actionTitle = "Every Action Movie Ever";
-    private static final String actionDescription = "The villain has left the hero for dead, or killed the hero’s brother, sister, parents, wife or family pets. Filled with righteous fury, the hero tools up and embarks on a bloody rampage.  \n \nIf the movie does well, we will do it again in the sequel! \n \nMaybe the villain tries to get revenge on the hero for getting revenge on the villain in this movie...";
-    private static final String actionYear = "2017";
-    private static final String actionImage = "genericaction";
-    private static final String actionWeblink = "http://www.denofgeek.com/us/movies/18824/the-5-most-common-action-movie-plots";
+    private static final String gameTitle = "Call of Duty: Black Ops, Game";
+    private static final String gameDescription = "First-person shooter game developed by Treyarch and published by activision. First released in 2010 on pc and console. You can play online with and against friends, as well as endure an emaculate campaign. Lets not forget possibly the best part... ZOMBIES! ";
+    private static final String gameYear = "2010";
+    private static final String gameImage = "blackops";
+    private static final String gameWeblink = "https://www.callofduty.com/blackops";
 
+    private static final String songTitle = "Bang, Song";
+    private static final String songDescription = "Song by AJR";
+    private static final String songYear = "2020";
+    private static final String songImage = "ajr";
+    private static final String songWeblink = "https://www.youtube.com/watch?v=4THFRpw68oQ";
+
+    private static final String youtTitle = "Graham Stephan, YouTuber";
+    private static final String youtDescription = "Real estate agent and investor, talking about money advice and investment tactics. ";
+    private static final String youtYear = "2016";
+    private static final String youtImage = "yout";
+    private static final String youtWeblink = "https://www.youtube.com/c/GrahamStephan/featured";
+
+    private static final String appTitle = "FlappyBird, Game";
+    private static final String appDescription = "First of a kind mobile game that took the world by storm in 2013. The player controls a bird, trying to avoid incoming obstacles. My inspiration for computer science and app development.";
+    private static final String appYear = "2013";
+    private static final String appImage = "app";
+    private static final String appWeblink = "https://flappybird.io/";
 
     /**
      * Create and return an array of Movie items.  Duh!
      */
-    public List<MovieModel> createMovieMagic () {
+    public List<MovieModel> createMovieMagic() {
 
         // make those movie objects
-        MovieModel action = new MovieModel(actionTitle, actionDescription, actionYear, actionImage, actionWeblink);
-        MovieModel romcom = new MovieModel(movie1Title, movie1Description, movie1Year, movie1Image, movie1Weblink);
+
+        MovieModel fast = new MovieModel(fastTitle, fastDescription, fastYear, fastImage, fastWeblink);
+        MovieModel game = new MovieModel(gameTitle, gameDescription, gameYear, gameImage, gameWeblink);
+        MovieModel song = new MovieModel(songTitle, songDescription, songYear, songImage, songWeblink);
+        MovieModel yout = new MovieModel(youtTitle, youtDescription, youtYear, youtImage, youtWeblink);
+        MovieModel app = new MovieModel(appTitle, appDescription, appYear, appImage, appWeblink);
+
 
         // add EACH movie object to our lists and maps
-        addMovieToList(action);
-        addMovieToList(romcom);
 
+        addMovieToList(fast);
+        addMovieToList(game);
+        addMovieToList(song);
+        addMovieToList(yout);
+        addMovieToList(app);
         // no more movies to add?  Okay... return our list
         return MOVIES;
     }
 
+
     // Internal helper so we don't forget any steps in the complex two-step system.  Seriously.  It happens.
-    private void addMovieToList (MovieModel datMovie) {
+    private void addMovieToList(MovieModel datMovie) {
         MOVIES.add(datMovie);
         ITEM_MAP.put(datMovie.getMovieTitle(), datMovie);
     }
