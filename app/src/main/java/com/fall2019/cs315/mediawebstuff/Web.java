398b84ff3844815f6294aa2606f9de287adfb6df
Julio Lopez <Juliol2540@gmail.com>
2020-09-29 20:30:32 -0500

    Added last touches. Everything is done :)

--- app/src/main/java/com/fall2019/cs315/mediawebstuff/Web.java
+++ app/src/main/java/com/fall2019/cs315/mediawebstuff/Web.java
@@ -1,24 +1,43 @@
 package com.fall2019.cs315.mediawebstuff;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class Web extends AppCompatActivity {
+
+    public ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.web_activity);
 
+        progressBar = (ProgressBar)findViewById(R.id.pro);
+
         Bundle extras = getIntent().getExtras();
 
         final WebView web = (WebView) findViewById(R.id.web);
         web.getSettings().setJavaScriptEnabled(true);
-        web.setWebViewClient(new WebViewClient());
+        web.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+                setTitle("Loading...");
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+                setTitle(view.getTitle());
+            }
+        });
 
         String URL = "";
 
